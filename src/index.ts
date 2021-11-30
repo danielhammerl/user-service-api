ac14ca4b5ed32c9fe97bc1465def38f411662383
Daniel Hammerl <daniel.hammerl99@gmx.de>
2021-11-30 20:26:00 +0100

    add new permission

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export enum Permission {
   ADMIN = 'admin',
   READ_USER = 'readUser',
   REGISTER_TO_PI_MONITORING_SERVER = 'register-to-pi-monitoring-server',
+  READ_PI_MONITORING_STATUS = 'read-pi-monitoring-status',
 }
 
 export interface User {
